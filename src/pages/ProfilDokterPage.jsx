8371eab5b7d972d9b4bf97594ab2d4de469c018c
Ardeya Windyana <ardeya.windy02@gmail.com>
2023-06-16 21:38:50 +0700

    profil

--- src/pages/ProfilDokterPage.jsx
+++ src/pages/ProfilDokterPage.jsx
@@ -1,29 +1,18 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 import NavbarComponent from "../component/Navbar";
 import Footer from "../component/Footer";
 import { DokterProvider } from "../context/DokterContext";
 import ProfileDokter from "../component/ProfilDokter";
 
 function ProfilDokterPage() {
-  const navigate = useNavigate();
-  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true"; // Periksa status login
-
-  useEffect(() => {
-    if (!isLoggedIn) {
-      // Jika pengguna belum login, arahkan ke halaman login
-      navigate("/login");
-    }
-  }, [isLoggedIn, navigate]);
-
   return (
     <>
       <NavbarComponent />
-      {isLoggedIn && ( // Tampilkan profil dokter hanya jika pengguna sudah login
-        <DokterProvider>
-          <ProfileDokter />
-        </DokterProvider>
-      )}
+
+      <DokterProvider>
+        <ProfileDokter />
+      </DokterProvider>
+
       <Footer />
     </>
   );
